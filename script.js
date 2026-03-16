842e75ce12bd9b662851b87ea7c5bd271cecd6b0
Alejandro Polo <polodiazalejandro2@gmail.com>
2026-03-16 11:52:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-// Exercise 1 Comparador de Textos -->
-const boton = document.getElementById('compareBtn');
-boton.addEventListener('click', compareStrings);
-function compareStrings() {
-    const text1 = document.getElementById('text1').value;
-    const text2 = document.getElementById('text2').value;
-    const result = document.getElementById('result');
-
-    if (text1 === text2) {
-        result.textContent = "Los textos son iguales.";
-    } else {
-        result.textContent = "Los textos son diferentes.";
     // Exercise 1 Comparador de Textos -->
 const boton = document.getElementById('compareBtn');
 boton.addEventListener('click', compareStrings);
@@ -77,49 +65,6 @@ function cambiacolorF() {
     }
 }
 // Exercise 5 Altura y ancho de una imagen -->
-function cambiatamaño() {
-    let ancho = document.getElementById("width").value;
-    let alto = document.getElementById("height").value;
-    let fihimg = document.getElementById("image");
-    if (alto > 400) {
-        alto = 400
-    }
-    if (ancho > 800) {
-        ancho = 800
-    }
-    fihimg.style.height = alto + "px";
-    fihimg.style.width = ancho + "px";
-
-}
-// Exercise 6 Gestion de los pedidos de una cafeteria -->
-
-}
-// Exercise 2 Contador de letras -->
-const countBtn = document.getElementById('countBtn');
-countBtn.addEventListener('click', countLetters);
-function countLetters() {
-    const text = document.getElementById('textToCount').value;
-    const countResult = document.getElementById('countResult');
-    countResult.textContent = `El texto tiene ${text.length} letras.`;
-}
-// Exercise 3 Raiz Cuadrada -->
-const sqrtBtn = document.getElementById('sqrtBtn');
-sqrtBtn.addEventListener('click', calculateSqrt);
-function calculateSqrt() {
-    const number = parseFloat(document.getElementById('numberInput').value);
-    const sqrtResult = document.getElementById('sqrtResult');
-
-    if (number < 0) {
-        sqrtResult.textContent = "No se puede calcular la raíz cuadrada de un número negativo.";
-    }else if (isNaN(number)) {
-        sqrtResult.textContent = "Por favor, ingrese un número válido.";
-    } 
-    else {
-        sqrtResult.textContent = `La raíz cuadrada de ${number} es ${Math.sqrt(number)}.`;
-    }
-}
-// Exercise 4 Cambio de color -->
-// Exercise 5 Altura y ancho de una imagen -->
 function cambiatamaño(){
     let ancho = document.getElementById("width").value;
     let alto = document.getElementById("height").value;
@@ -132,6 +77,6 @@ function cambiatamaño(){
     }
     fihimg.style.height = alto + "px";
     fihimg.style.width = ancho + "px";
-
 }
-// Exercise 6 Gestion de los pedidos de una cafeteria -->
+// Exercise 6 Gestión de los pedidos de una cafetería -->
+
